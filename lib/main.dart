d90a6bd5fd139bb0e151e8b21af512c67ae91246
Mangoo0110 <anik15@cse.pstu.ac.bd>
2026-03-09 02:05:08 +0600

    3d to 2d(screen) projection basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:math_and_3d/core/themes/themes.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -24,9 +28,38 @@ class MyApp extends StatelessWidget {
 }
 
 
-class TheRealm extends StatelessWidget {
+class TheRealm extends StatefulWidget {
   const TheRealm({super.key, required this.screenSize});
   final Size screenSize;
+
+  @override
+  State<TheRealm> createState() => _TheRealmState();
+}
+
+class _TheRealmState extends State<TheRealm> {
+  Timer? timer;
+  double depth = 1;
+
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      depth += 0.1;
+      if(depth >= 10) {
+        timer.cancel();
+        return;
+      }
+      setState(() {
+        
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,34 +68,39 @@ class TheRealm extends StatelessWidget {
         return Scaffold(
           body: Container(
             color: Colors.black,
-            height: screenSize.height,
-            width: screenSize.width,
+            height: widget.screenSize.height,
+            width: widget.screenSize.width,
             child: Stack(
               fit: StackFit.expand,
               children: [
+                Positioned(
+                  top: 10,
+                  left: 0,
+                  child: Text(depth.toString(), style: const TextStyle(color: Colors.white),),
+                ),
                   Positioned(
-                    left: screenSize.width/2,
+                    left: widget.screenSize.width/2,
                     child: Container(
-                      height: screenSize.height,
+                      height: widget.screenSize.height,
                       width: 1,
                       color: Colors.white,
                     ),
                   ),
                     
                 drawRectPoint(
-                  translateToScreenPoint(Vec2(.5, .5))
+                  translateToScreenPoint(project3dTo2d(Vec3(.5, .5, depth)))
                 ),
 
                 drawRectPoint(
-                  translateToScreenPoint(Vec2(.5, -.5))
+                  translateToScreenPoint(project3dTo2d(Vec3(.5, -.5, depth)))
                 ),
 
                 drawRectPoint(
-                  translateToScreenPoint(Vec2(-.5, .5))
+                  translateToScreenPoint(project3dTo2d(Vec3(-.5, .5, depth)))
                 ),
 
                 drawRectPoint(
-                  translateToScreenPoint(Vec2(-.5, -.5))
+                  translateToScreenPoint(project3dTo2d(Vec3(-.5, -.5, depth)))
                 ),
               ],
             ),
@@ -71,13 +109,31 @@ class TheRealm extends StatelessWidget {
       },
     );
   }
+  // Simplified formula from: 3d: (x, y, z) => 2d: (x/z, y/z)
+  // Which is essentially derived from similar triangle theorem
+  //
+  /// Below videos sources explains the formula behind 3d coordinates to 2d coordinates much better: 
+  /// https://youtu.be/eoXn6nwV694?si=hOaW5FIAczj45jzD
+  /// https://youtu.be/qjWkNZ0SXfo?si=gYZLzm_ht9McmHW7
+  ///
+  // More detailed formula is: 
+  // 3d: (x, y, z) => 2d: (x/(z * tan(theta)), y/(z * tan(theta))) 
+  //
+  // Here z is the depth or how far behind the object is from the screen.
+  // Minimum depth z is 0.1 to avoid division by zero
+  Vec2 project3dTo2d(Vec3 coooridanates) {
+    return Vec2(
+      coooridanates.x / (max(0.1, coooridanates.z)),
+      coooridanates.y / (max(0.1, coooridanates.z))
+    );
+  }
 
   Vec2 translateToScreenPoint(Vec2 coordinate) {
     // Assuming that coordinate values are between -1 and 1
     // (0,0) is the middle point of the plane
     return Vec2(
-      (1 - coordinate.x)/2 * screenSize.width,
-      (1 -(coordinate.y))/2 * screenSize.height
+      (1 - coordinate.x)/2 * widget.screenSize.width,
+      (1 - coordinate.y)/2 * widget.screenSize.height
     );
   }
 }
@@ -116,6 +172,31 @@ class Vec2{
 
   @override
   int get hashCode => x.hashCode ^ y.hashCode;
+}
+
+/// 3 dimensional vector
+class Vec3{
+  Vec3(this.x, this.y, this.z);
+
+  final double x;
+  final double y;
+  final double z;
+
+  Vec3 operator +(Vec3 other) => Vec3(x + other.x, y + other.y, z + other.z);
+  Vec3 operator -(Vec3 other)=> Vec3(x- other.x, y - other.y, z - other.z);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) 
+      && other is Vec3
+      &&  runtimeType ==  other.runtimeType
+      && x == other.x
+      && y == other.y
+      && z == other.z;
+  }
+
+  @override
+  int get hashCode => x.hashCode ^ y.hashCode ^ z.hashCode;
 }
 
 
